047900bddef2f81516671fdbf833dbcbe1fa3d99
reinamura017 <reina923@gmail.com>
2020-01-02 10:36:53 +0900

    Add account activation

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   get    '/signup',  to: 'users#new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
-  post    '/signup',  to: 'users#create'
   delete '/logout',  to: 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
